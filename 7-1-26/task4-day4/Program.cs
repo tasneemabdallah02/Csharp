360ae2283544cf752126afffa47ab56b74d1c0ab
tasneemabdallah02 <tasneemabdallah02@gmail.com>
2026-01-07 11:43:11 +0300

    task4

---
+++ 7-1-26/task4-day4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace task4_day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string InputFile = "Employee.txt";
+           
+            string[] lines = File.ReadAllLines(InputFile);
+            {
+                Console.WriteLine("Employee Information:");
+                Console.WriteLine("----------------------");
+
+                foreach (string line in lines)
+                {
+                    string[] Data = line.Split('\\');
+                    string Name = Data[0];
+                    string Salary = Data[1];
+                    string Department = Data[2];
+                    double salary = double.Parse(Salary);
+                    Console.WriteLine("Name : " + Name + "\n" + "Department : " + Department + "\n" + "Salary : " + Salary + "\n");
+                }
+                string OutputFile = "result.txt";
+                File.WriteAllText(OutputFile, "\n");
+                foreach (string line in lines)
+                    {
+                        string[] Data = line.Split('\\');
+
+                        string Name = Data[0];
+                        string Department = Data[1];
+                        string Salary = Data[2];
+
+                        double slary = double.Parse(Salary);
+                    string resultLine = $"{Name} Work At {Department} with salary {Salary} years old";
+                    File.AppendAllText(OutputFile, resultLine + "\n");
+                }
+            }
+        }
+    }
+}
